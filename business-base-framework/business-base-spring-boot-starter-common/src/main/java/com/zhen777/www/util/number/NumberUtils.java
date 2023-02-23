a980adfcd6b1a36aa1eaae7269628ad523f51c0b
张亮 <zhangl02@szlanyou.com>
2023-02-23 12:00:36 +0800

    基础包完成

---
+++ business-base-framework/business-base-spring-boot-starter-common/src/main/java/com/zhen777/www/util/number/NumberUtils.java
@@ -0,0 +1,19 @@
+package com.zhen777.www.util.number;
+
+import cn.hutool.core.util.StrUtil;
+
+/**
+ * 数字的工具类，补全 {@link cn.hutool.core.util.NumberUtil} 的功能
+ * <p>
+ * .
+ *
+ * @author mikoozhang[zhangliang2198@outlook.com]
+ * @version 1.0.0
+ */
+public class NumberUtils {
+
+    public static Long parseLong(String str) {
+        return StrUtil.isNotEmpty(str) ? Long.valueOf(str) : null;
+    }
+
+}
---
+++ business-base-framework/business-base-spring-boot-starter-common/src/main/java/com/zhen777/www/validation/Mobile.java
@@ -0,0 +1,39 @@
+package com.zhen777.www.validation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+
+/**
+ * 移动.
+ *
+ * @author mikoozhang[zhangliang2198@outlook.com]
+ * @version 1.0.0
+ */
+@Target({
+        ElementType.METHOD,
+        ElementType.FIELD,
+        ElementType.ANNOTATION_TYPE,
+        ElementType.CONSTRUCTOR,
+        ElementType.PARAMETER,
+        ElementType.TYPE_USE
+})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Constraint(
+        validatedBy = MobileValidator.class
+)
+public @interface Mobile {
+
+    String message() default "手机号格式不正确";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+}
