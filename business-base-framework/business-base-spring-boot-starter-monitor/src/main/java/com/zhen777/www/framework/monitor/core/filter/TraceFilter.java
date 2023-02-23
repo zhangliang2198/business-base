6db2414d8a0f18495f76e8f56c1582feb56bb040
张亮 <zhangl02@szlanyou.com>
2023-02-23 18:18:24 +0800

    结构调整

---
+++ business-base-framework/business-base-spring-boot-starter-monitor/src/main/java/com/zhen777/www/framework/monitor/core/filter/TraceFilter.java
@@ -0,0 +1,36 @@
+package com.zhen777.www.framework.monitor.core.filter;
+
+import com.zhen777.www.framework.common.util.monitor.TracerUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Trace 过滤器，打印 traceId 到 header 中返回
+ * <p>
+ * .
+ *
+ * @author mikoozhang[zhangliang2198@outlook.com]
+ * @version 1.0.0
+ */
+public class TraceFilter extends OncePerRequestFilter {
+
+    /**
+     * Header 名 - 链路追踪编号
+     */
+    private static final String HEADER_NAME_TRACE_ID = "trace-id";
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        // 设置响应 traceId
+        response.addHeader(HEADER_NAME_TRACE_ID, TracerUtils.getTraceId());
+        // 继续过滤
+        chain.doFilter(request, response);
+    }
+
+}
